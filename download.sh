5f872b26520157bda8d24dcca13cf2d3b8cadcd0
Max Thanachai Thongkum <singmax161@gmail.com>
2023-06-01 16:22:26 +0700

    add script

---
+++ download.sh
@@ -0,0 +1,3 @@
+# Read me https://www.hitachivantara.com/en-us/products/dataops-software/data-integration-analytics/pentaho-community-edition.html
+curl https://privatefilesbucket-community-edition.s3.us-west-2.amazonaws.com/9.4.0.0-343/ce/client-tools/pdi-ce-9.4.0.0-343.zip -o pdi.zip
+unzip pdi.zip
